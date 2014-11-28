aaa3fa0dc3f2dfe0cbba81a9d1ce3b25959c80f0
rodionovstepan <rodionov.stepan@gmail.com>
2014-11-29 02:58:49 +0600

    валидация email

--- js/lang.js
+++ js/lang.js
@@ -2,3 +2,4 @@ var aos = aos || {};
 aos.lang = aos.lang || {};
 
 aos.lang.username_and_email_are_required = 'Необходимо указать Имя пользователя и Email';
+aos.lang.invalid_email = 'Указан некорректный электронный адрес';
--- js/register.js
+++ js/register.js
@@ -11,9 +11,20 @@ aos.register = function() {
 		$('.form_validation').text(msg).show();
 	};
 
+	var validateEmail = function(email) { 
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email);
+	};
+
 	if (username.trim() == '' || email.trim() == '') {
 		showValidation(aos.lang.username_and_email_are_required);
-		username.focus();
 		return false;
 	}
+
+	if (!validateEmail(email)) {
+		showValidation(aos.lang.invalid_email);
+		return false;
+	}
+
+	
 };
