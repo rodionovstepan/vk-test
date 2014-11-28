ae88440e78814e33b146412ac42db526900ad026
rodionovstepan <rodionov.stepan@gmail.com>
2014-11-29 01:28:11 +0600

    нарисовалась какая-то форма для регистрации

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<title>Abstract Order System | VK Test</title>
+		<meta charset="utf-8"> 
 		<link rel="stylesheet" href="css/common.css">
 	</head>
 	<body>
@@ -20,12 +21,43 @@
 		<div class="page_container" id="content">
 			<div class="page_welcome">
 				<div class="page_welcome_title">
-					Welcome to AOS!
+					Добро пожаловать!
+				</div>
+				<div class="page_welcome_desc">
+					AOS &mdash; это система выполнения абстрактных заказов, где "исполнители" выполняют заказы "заказчиков".
+				</div>
+				<div class="page_welcome_register">
+					<div class="page_welcome_register_title">
+						<b>Присоединяйся</b>, пройдя очень простую процедуру регистрации:
+					</div>
+					<div class="page_welcome_register_form">
+						<div class="form_item">
+							<input type="text" name="username" placeholder="Имя пользователя">
+						</div>
+						<div class="form_item">
+							<input type="text" name="email" placeholder="Электронная почта">
+						</div>
+						<div class="form_item">
+							<input type="password" name="password" placeholder="Пароль">
+						</div>
+						<div class="form_item">
+							<input type="password" name="password_confirm" placeholder="Пароль еще раз">
+						</div>
+						<div class="form_item">
+							<select name="role">
+								<option value="1">Я - заказчик</option>
+								<option value="2">Я - исполнитель</option>
+							</select>
+						</div>
+						<div class="form_item">
+							<button type="button" class="main_button">Зарегистрироваться</button>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
 		<div class="page_footer">
-			<a href="https://vk.com/rodionovstepan" target="_blank">Rodionov Stepan</a> &copy; 2014
+			&copy; 2014
 		</div>
 	</body>
 </html>
