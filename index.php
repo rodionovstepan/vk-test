e0f3f22f11e9cbb86b8c1635c18e130411a2d67d
rodionovstepan <rodionov.stepan@gmail.com>
2014-11-28 23:52:07 +0600

    начал делать разметку

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <!doctype html>
 <html>
 	<head>
-		<title>VK Test</title>
+		<title>Abstract Order System | VK Test</title>
+		<link rel="stylesheet" href="css/common.css">
 	</head>
 	<body>
-		<?php echo "Hello World!" ?>
+		<div class="navbar">
+			<div class="page_container">
+				<div class="navbar_container">
+					<div class="navbar_left">
+						AOS | VK Test
+					</div>
+					<div class="navbar_right">
+						Test app
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="page_container" id="content">
+			<div class="page_welcome">
+				Welcome!
+			</div>
+		</div>
 	</body>
 </html>
