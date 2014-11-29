8f0181b96d4b781ff162cc5f2d520bf2552dcc82
rodionovstepan <rodionov.stepan@gmail.com>
2014-11-29 19:28:06 +0600

    кнопка для отмены заказа

--- backend/render.php
+++ backend/render.php
@@ -11,7 +11,10 @@
 			<div class=\"order_title\"><b>$title</b></div>
 			<div class=\"order_content\">$content</div>
 			<div class=\"order_footer\">
-				<div class=\"order_price\">$price</div>
+				<div class=\"order_price fl_l\">$price</div>
+				<div class=\"fl_r\">
+					<button type=\"button\" class=\"def_button\">Отменить заказ</button>
+				</div>
 			</div>
 		</div>";
 	}
