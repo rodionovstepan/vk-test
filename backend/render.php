4be1c1ea75a54d2c784aa96541f9e9e7da24093d
rodionovstepan <rodionov.stepan@gmail.com>
2014-11-30 02:56:58 +0600

    показывать комиссию

--- backend/render.php
+++ backend/render.php
@@ -3,7 +3,7 @@
 	function render_order($order, $is_customer, $context_user_id) {
 		$title = $order['title'];
 		$content = nl2br($order['content']);
-		$price = $order['price'];
+		$price = $order['price'] . ' (- комиссия ' . SYSTEM_PERCENT . '%)';
 		$id = $order['id'];
 		$customer_name = $order['customer_name'];
 		$customer_id = $order['customer_id'];
@@ -17,7 +17,7 @@
 
 		echo "
 		<div class=\"order\" id=\"order$id\">
-			<div class=\"order_title\"><b>$title</b> добавил <a href=\"customer.php?id=$customer_id\">$customer_name</a></div>
+			<div class=\"order_title\"><b>$title</b> от <a href=\"customer.php?id=$customer_id\">$customer_name</a></div>
 			<div class=\"order_content\">$content</div>
 			<div class=\"order_footer\">
 				<div class=\"order_price fl_l\">Вознаграждение: <b>$price</b></div>
