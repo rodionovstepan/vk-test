2c41019631cb5fca380a3365b34121a15a97b2b9
rodionovstepan <rodionov.stepan@gmail.com>
2014-11-29 14:39:43 +0600

    имя пользователя тоже положить в сессию

--- backend/auth.php
+++ backend/auth.php
@@ -3,19 +3,22 @@
 	require_once('context.php');
 
 	function login_user($user) {
-		if (isset($context_user_id, $context_user_role)) {
+		if (isset($context_user_id, $context_user_role, $context_user_name)) {
 			return;
 		}
 
 		$_SESSION['uid'] = $user['id'];
+		$_SESSION['uname'] = $user['username'];
 		$_SESSION['urole'] = $user['role'];
 	}
 
 	function logout_user() {
 		unset($context_user_id);
 		unset($context_user_role);
+		unset($context_user_name);
 		unset($_SESSION['uid']);
 		unset($_SESSION['urole']);
+		unset($_SESSION['uname']);
 		session_destroy();
 	}
 
--- backend/context.php
+++ backend/context.php
@@ -4,4 +4,5 @@
 	if (!empty($_SESSION['uid']) && !empty($_SESSION['urole'])) {
 		$context_user_id = intval($_SESSION['uid']);
 		$context_user_role = intval($_SESSION['urole']);
+		$context_user_name = $_SESSION['uname'];
 	}
