f538b1ebe10a92a55c27d515d88757cd4d0a1908
rodionovstepan <rodionov.stepan@gmail.com>
2014-11-30 13:18:37 +0600

    cleanUP

--- backend/render.php
+++ backend/render.php
@@ -3,7 +3,8 @@
 	function render_order($order, $is_customer, $context_user_id) {
 		$title = $order['title'];
 		$content = nl2br($order['content']);
-		$price = $order['price'] . ' (- комиссия ' . SYSTEM_PERCENT . '%)';
+		$price = $order['price'];
+		$aos_portion = '(' . SYSTEM_PERCENT . '% комиссия)';
 		$id = $order['id'];
 		$customer_name = $order['customer_name'];
 		$customer_id = $order['customer_id'];
@@ -20,7 +21,7 @@
 			<div class=\"order_title\"><b>$title</b> от <a href=\"customer.php?id=$customer_id\">$customer_name</a></div>
 			<div class=\"order_content\">$content</div>
 			<div class=\"order_footer\">
-				<div class=\"order_price fl_l\">Вознаграждение: <b>$price</b></div>
+				<div class=\"order_price fl_l\">Вознаграждение: <b>$price</b> $aos_portion</div>
 				<div class=\"fl_r\">" . $btn . "</div>
 			</div>
 		</div>";
--- orders.php
+++ orders.php
@@ -9,16 +9,20 @@
 	}
 
 	if ($act == 'add_order') {
+
 		$title = trim(mysql_real_escape_string(htmlspecialchars($_POST['title'])));
 		$content = trim(mysql_real_escape_string(htmlspecialchars($_POST['content'])));
 		$price = $_POST['price'];
 
 		echo json_encode(add_order_handler($title, $content, $price));
 	} else if ($_POST['act'] == 'cancel_order') {
+
 		echo json_encode(cancel_order_handler(intval($_POST['oid'])));
 	} else if ($_POST['act'] == 'take_order') {
+
 		echo json_encode(take_order_handler(intval($_POST['oid'])));
 	} else {
+		
 		invalid_request();
 	}
 
