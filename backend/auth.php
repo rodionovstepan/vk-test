ca233dd6a7036fb724b6d38bf46e3510c97de2c7
rodionovstepan <rodionov.stepan@gmail.com>
2014-12-03 14:40:30 +0600

    при авторизации генерируем токен, в заголовках всех ajax запросов будем его отправлять

--- backend/auth.php
+++ backend/auth.php
@@ -6,20 +6,35 @@
 		$_SESSION['uid'] = $user['id'];
 		$_SESSION['uname'] = $user['username'];
 		$_SESSION['urole'] = $user['role'];
+		$_SESSION['utoken'] = _generate_token();
 	}
 
 	function logout_user() {
-		if (!empty($_SESSION['uid']) && !empty($_SESSION['urole']) && !empty($_SESSION['uname'])) {
+		if (!empty($_SESSION['uid']) && !empty($_SESSION['urole']) && !empty($_SESSION['uname']) && !empty($_SESSION['utoken'])) {
 			unset($context_user_id);
 			unset($context_user_role);
 			unset($context_user_name);
+			unset($context_user_token);
 
 			unset($_SESSION['uid']);
 			unset($_SESSION['urole']);
 			unset($_SESSION['uname']);
-			
+			unset($_SESSION['utoken']);
+
 			session_destroy();
 		}
+	}
+
+	function _generate_token() {
+		$alpha = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$length = strlen($alpha);
+		$token = '';
+		
+		for ($i = 0; $i < 10; $i++) {
+		  $token .= $alpha[rand(0, $length - 1)];
+		}
+
+		return md5(md5($token));
 	}
 
 ?>
--- backend/context.php
+++ backend/context.php
@@ -10,13 +10,14 @@
 
 	session_start();
 
-	if (!empty($_SESSION['uid']) && !empty($_SESSION['urole']) && !empty($_SESSION['uname'])) {
+	if (!empty($_SESSION['uid']) && !empty($_SESSION['urole']) && !empty($_SESSION['uname']) && !empty($_SESSION['utoken'])) {
 		$context_user_id = intval($_SESSION['uid']);
 		$context_user_role = intval($_SESSION['urole']);
 		$context_user_name = $_SESSION['uname'];
+		$context_user_token = $_SESSION['utoken'];
 	}
 
 	$is_ajax = !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && 
-				strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
+					strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
 
 ?>
